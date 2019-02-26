1ff0ac11c192e9dea4cf0e91e3f9f104fbc5d9cb
yelangfeng <langfeng_ye@qq.com>
2019-02-26 23:36:50 +0800

    增加一级缓存内容

--- src/cn/csdas/yelf/day02/HibernateDemo.java
+++ src/cn/csdas/yelf/day02/HibernateDemo.java
@@ -5,6 +5,7 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.junit.Test;
 
+import javax.sound.midi.Soundbank;
 import java.io.Serializable;
 
 /**
@@ -53,5 +54,47 @@ public class HibernateDemo {
 
         System.out.println(account);  //脱管态对象，有唯一标识OID，没有被session管理
 
+    }
+    @Test
+    //持久态对象特性测试
+    public void demo3(){
+        Session session = HibernateUtils.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        Account account = session.get(Account.class,1);
+        account.setMoney(555);//持久化对象即使数据有变化，即使后面无更新语句Hibernate也会发送更新语句到数据库
+
+        transaction.commit();
+        session.close();
+
+    }
+
+    @Test
+    //证明一级缓存的存在
+    public void demo4(){
+        Session session = HibernateUtils.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        /*
+        //证明一
+        Account account = session.get(Account.class,1);//发送sql
+        System.out.println(account);
+        Account account1 = session.get(Account.class,1);//不发送sql
+        System.out.println(account1);
+        System.out.println(account1==account);
+        */
+        //证明二
+        Account account = new Account();
+        account.setId(2);
+        account.setName("yelf");
+        account.setMoney(222);
+        Serializable id = session.save(account);//发送sql
+        Account account1 = session.get(Account.class, id);//不发送sql
+        System.out.println(account1);
+
+        transaction.commit();
+        session.close();
+
+        //一级缓存快照区：当事物要提交时，一级缓存的数据（持久化对象）与快照区的数据作对比，不一致时自动发送执行update
     }
 }
