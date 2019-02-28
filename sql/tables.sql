9860369f6906268335e5334ef6286131171583d2
yelangfeng <langfeng_ye@qq.com>
2019-02-28 23:04:59 +0800

    增加day03内容

---
+++ sql/tables.sql
@@ -0,0 +1,23 @@
+CREATE TABLE  cst_customer  (
+   cust_id  int identity(1,1) primary key,
+   cust_name  varchar(32) NOT NULL ,
+   cust_source  varchar(32) DEFAULT NULL ,
+   cust_industry  varchar(32) DEFAULT NULL,
+   cust_level  varchar(32) DEFAULT NULL,
+   cust_phone  varchar(64) DEFAULT NULL,
+   cust_mobile  varchar(16) DEFAULT NULL,
+)
+
+CREATE TABLE  cst_linkman  (
+   lkm_id  int identity(1,1) primary key,
+   lkm_name  varchar(16) DEFAULT NULL,
+   lkm_cust_id  int DEFAULT NULL,
+   lkm_gender  char(1) DEFAULT NULL,
+   lkm_phone  varchar(16) DEFAULT NULL,
+   lkm_mobile  varchar(16) DEFAULT NULL,
+   lkm_email  varchar(64) DEFAULT NULL,
+   lkm_qq  varchar(16) DEFAULT NULL,
+   lkm_position  varchar(16) DEFAULT NULL,
+   lkm_memo  varchar(512) DEFAULT NULL,
+   foreign key(lkm_cust_id) references cst_customer(cust_id)
+   )
