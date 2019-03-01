c97d77d7bb3f63e3b9a13f82afd3a9a57bd12992
yelangfeng <langfeng_ye@qq.com>
2019-03-01 11:14:42 +0800

    hibernate框架一对多测试

--- src/cn/csdas/yelf/day03/HibernateDemo.java
+++ src/cn/csdas/yelf/day03/HibernateDemo.java
@@ -10,12 +10,18 @@ import org.junit.Test;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * 一对多测试类
+ */
 public class HibernateDemo {
     @Test
+    //保存2个客户和3个联系人
     public void demo1(){
+        //创建session和开启事务
         Session session = HibernateUtils.getCurrentSession();
         Transaction transaction = session.beginTransaction();
 
+        //创建两个客户
         Customer customer1 = new Customer();
         Customer customer2 = new Customer();
         customer1.setCust_id(1L);
@@ -23,6 +29,7 @@ public class HibernateDemo {
         customer1.setCust_name("张三");
         customer2.setCust_name("李四");
 
+        //创建3个联系人
         LinkMan linkMan1 = new LinkMan();
         LinkMan linkMan2 = new LinkMan();
         LinkMan linkMan3 = new LinkMan();
@@ -33,6 +40,7 @@ public class HibernateDemo {
         linkMan2.setLkm_name("赵六");
         linkMan3.setLkm_name("王八");
 
+        //设置关系
         linkMan1.setCustomer(customer1);
         linkMan2.setCustomer(customer2);
         linkMan3.setCustomer(customer2);
@@ -40,6 +48,7 @@ public class HibernateDemo {
         customer2.getLinkMans().add(linkMan2);
         customer2.getLinkMans().add(linkMan3);
 
+        //保存数据
         session.save(customer1);
         session.save(customer2);
         session.save(linkMan1);
