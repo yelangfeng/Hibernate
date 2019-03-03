2536b56da4d216b4abc2d16e550e3fa736fa27b6
yelangfeng <langfeng_ye@qq.com>
2019-03-03 13:21:05 +0800

    增加day03内容-User and Role

--- src/cn/csdas/yelf/day03/demo2/HIbernateDemo.java
+++ src/cn/csdas/yelf/day03/demo2/HIbernateDemo.java
@@ -55,4 +55,175 @@ public class HIbernateDemo {
 
         tx.commit();
     }
+
+    @Test
+    /**
+     * 多对多的操作：
+     * * 只保存一边是否可以？不可以，瞬时对象异常
+     */
+    public void demo2(){
+        Session session = HibernateUtils.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+
+        // 创建2个用户
+        User user1 = new User();
+        user1.setUser_name("赵洪");
+
+        // 创建3个角色
+        Role role1 = new Role();
+        role1.setRole_name("研发部");
+
+        // 设置双向的关联关系:
+        user1.getRoles().add(role1);
+        role1.getUsers().add(user1);
+        // 只保存用户：
+         session.save(user1);
+        // 只保存角色：
+        //session.save(role1);
+
+        tx.commit();
+    }
+
+    @Test
+    /**
+     * 多对多的级联保存：
+     * * 保存用户级联保存角色。在用户的映射文件中配置。
+     * * 在User.hbm.xml中的set上配置 cascade="save-update"
+     */
+    public void demo3(){
+        Session session = HibernateUtils.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        Role role1 = new Role();
+        User user1 = new User();
+        user1.setUser_name("张三");
+        role1.setRole_name("公关部");
+
+        // 设置双向的关联关系:
+        user1.getRoles().add(role1);
+        role1.getUsers().add(user1);
+        // 保存用户：
+        session.save(user1);
+
+        transaction.commit();
+        session.close();
+    }
+
+    /**
+     * 多对多的级联保存：
+     * * 保存角色级联保存用户。在角色的映射文件中配置。
+     * * 在Role.hbm.xml中的set上配置 cascade="save-update"
+     */
+    @Test
+    public void demo4(){
+        Session session = HibernateUtils.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+
+        // 创建2个用户
+        User user1 = new User();
+        user1.setUser_name("李兵");
+
+        // 创建3个角色
+        Role role1 = new Role();
+        role1.setRole_name("公关部");
+
+        // 设置双向的关联关系:
+        user1.getRoles().add(role1);
+        role1.getUsers().add(user1);
+        // 只保存用户：
+        session.save(role1);
+
+        tx.commit();
+    }
+
+    /**
+     * 多对多的级联删除：
+     * * 删除用户级联删除角色
+     * * 在User.hbm.xml中的set上配置 cascade="delete"
+     */
+    @Test
+    public void demo5(){
+        Session session = HibernateUtils.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+
+        // 查询1号用户:
+        User user  = session.get(User.class, 1);
+        session.delete(user);
+
+        tx.commit();
+    }
+
+    /**
+     * 多对多的级联删除：
+     * * 删除角色级联删除用户
+     * * 在Role.hbm.xml中的set上配置 cascade="delete"
+     */
+    @Test
+    public void demo6(){
+        Session session = HibernateUtils.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+
+        // 查询2号角色:
+        Role role  = session.get(Role.class, 1);
+        session.delete(role);
+
+        tx.commit();
+    }
+
+    @Test
+    /**
+     * 给用户选择角色
+     */
+    public void demo7(){
+        Session session = HibernateUtils.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+
+        // 给1号用户多选2号角色
+        // 查询1号用户
+        User user  = session.get(User.class, 1);
+        // 查询2号角色
+        Role role = session.get(Role.class, 2);
+        user.getRoles().add(role);
+
+        tx.commit();
+    }
+
+    @Test
+    /**
+     * 给用户改选角色
+     */
+    public void demo8(){
+        Session session = HibernateUtils.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+
+        // 给2号用户将原有的2号角色改为3号角色
+        // 查询2号用户
+        User user  = session.get(User.class, 2);
+        // 查询2号角色
+        Role role2 = session.get(Role.class, 2);
+        Role role3 = session.get(Role.class, 3);
+        user.getRoles().remove(role2);
+        user.getRoles().add(role3);
+
+        tx.commit();
+    }
+
+    @Test
+    /**
+     * 给用户删除角色
+     */
+    public void demo9(){
+        Session session = HibernateUtils.getCurrentSession();
+        Transaction tx = session.beginTransaction();
+
+        // 给2号用户删除1号角色
+        // 查询2号用户
+        User user  = session.get(User.class, 2);
+        // 查询2号角色
+        Role role = session.get(Role.class, 1);
+        user.getRoles().remove(role);
+
+        tx.commit();
+    }
+
 }
